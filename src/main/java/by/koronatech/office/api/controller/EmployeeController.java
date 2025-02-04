688e281e61d285802218e0dd25792140f7b16a48
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-02-05 00:11:28 +0300

    add employee controller

--- src/main/java/by/koronatech/office/api/controller/EmployeeController.java
+++ src/main/java/by/koronatech/office/api/controller/EmployeeController.java
@@ -1,4 +1,32 @@
 package by.koronatech.office.api.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
+    @GetMapping("/{department}")
+    public String getEmployeesByDepartment(@PathVariable String department){
+        return "";
+    }
+
+    @PostMapping("/{department}")
+    public String addEmployeeToDepartment(@PathVariable String department,@RequestParam String employee){
+        return "";
+    }
+    @PatchMapping("/{id}")
+    public String addToEmployeeManagerStatus(@RequestParam String employee){
+        return "";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateEmployeeInformation(@PathVariable String id, @RequestParam String employee){
+        return "";
+    }
+
+    @DeleteMapping("/{department}")
+    public String deleteEmployeeFromDepartment(@PathVariable String department, @RequestParam String employee){
+        return "";
+    }
 }
