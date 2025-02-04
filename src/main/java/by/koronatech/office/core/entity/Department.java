b4a2db45953bf9f8d1c2bbcc4a42dcccb18d9580
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-02-05 00:21:47 +0300

    add department entity

---
+++ src/main/java/by/koronatech/office/core/entity/Department.java
@@ -0,0 +1,16 @@
+package by.koronatech.office.core.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    private long id;
+
+    private String name;
+
+}
