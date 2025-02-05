cfde8f01f21ed53c2be7338e1d34cd3e1c25bac3
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-02-06 00:47:18 +0300

    add database connection

---
+++ src/main/java/by/koronatech/office/core/mapper/office/CreateEmployeeMapper.java
@@ -0,0 +1,10 @@
+package by.koronatech.office.core.mapper.office;
+
+import by.koronatech.office.api.dto.CreateEmployeeDTO;
+import by.koronatech.office.core.entity.Employee;
+import by.koronatech.office.core.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+
+@Mapper(config = BaseMapper.class)
+public interface CreateEmployeeMapper extends  BaseMapper<Employee, CreateEmployeeDTO> {
+}
--- src/main/java/by/koronatech/office/core/service/serviceImpl/EmployeeServiceImpl.java
+++ src/main/java/by/koronatech/office/core/service/serviceImpl/EmployeeServiceImpl.java
@@ -2,21 +2,39 @@ package by.koronatech.office.core.service.serviceImpl;
 
 import by.koronatech.office.api.dto.CreateEmployeeDTO;
 import by.koronatech.office.api.dto.GetEmployeeDTO;
+import by.koronatech.office.core.entity.Employee;
+import by.koronatech.office.core.mapper.office.CreateEmployeeMapper;
+import by.koronatech.office.core.mapper.office.EmployeeMapper;
 import by.koronatech.office.core.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private final List<Employee> employeeRepository = new ArrayList<>(
+            List.of(
+                    Employee.builder().id(1L).department("HR").name("John").manager(false).build(),
+                    Employee.builder().id(2L).department("accounting").name("Ivan").manager(true).build()
+            )
+    );
+
+    private EmployeeMapper employeeMapper;
+    private CreateEmployeeMapper createEmployeeMapper;
+
     @Override
     public List<GetEmployeeDTO> findEmployeeByDepartment(String department) {
-        return List.of();
+        return employeeMapper.toDtos(employeeRepository.stream().filter(employee -> employee.getDepartment().equals(department)).toList());
     }
 
     @Override
     public GetEmployeeDTO addEmployeeToDepartment(CreateEmployeeDTO createEmployeeDTO) {
-        return null;
+        employeeRepository.add(createEmployeeMapper.toEntity(createEmployeeDTO));
+        return employeeMapper.toDto(createEmployeeMapper.toEntity(createEmployeeDTO));
     }
 
     @Override
@@ -32,5 +50,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public GetEmployeeDTO deleteEmployee(long id) {
         return null;
+    }
+
+    public GetEmployeeDTO findById(long id){
+        return employeeMapper.toDto(employeeRepository.stream().filter(employee -> employee.getId().equals(id)).findFirst().get());
     }
 }
