a710f373fd906781e2a791b7f052cb10195367ec
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-02-05 07:10:32 +0300

    add department repository

--- src/main/java/by/koronatech/office/core/service/DepartmentService.java
+++ src/main/java/by/koronatech/office/core/service/DepartmentService.java
@@ -3,9 +3,11 @@ package by.koronatech.office.core.service;
 import by.koronatech.office.api.dto.GetDepartmentDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface DepartmentService {
 
-    GetDepartmentDTO getDepartments();
+   List<GetDepartmentDTO> getDepartments();
 
 }
--- src/main/java/by/koronatech/office/core/service/serviceImpl/DepartmentServiceImpl.java
+++ src/main/java/by/koronatech/office/core/service/serviceImpl/DepartmentServiceImpl.java
@@ -2,14 +2,25 @@ package by.koronatech.office.core.service.serviceImpl;
 
 
 import by.koronatech.office.api.dto.GetDepartmentDTO;
+import by.koronatech.office.core.entity.Department;
 import by.koronatech.office.core.service.DepartmentService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
 
+    private final List<Department> departmentRepository = new ArrayList<>(
+            List.of(
+                    Department.builder().id(1l).name("HR").build(),
+                    Department.builder().id(2L).name("accounting").build()
+            )
+    );
+
     @Override
-    public GetDepartmentDTO getDepartments() {
-        return null;
+    public List<GetDepartmentDTO> getDepartments() {
+        return departmentRepository;
     }
 }
